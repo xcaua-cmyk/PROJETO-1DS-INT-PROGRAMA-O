595270fdc103a4eadc8be07e4141b507dc8af8b0
xcaua-cmyk <x.caua@escola.pr.gov.br>
2026-08-06 11:47:05 -0300

    Add files via upload

---
+++ main(3).py
@@ -0,0 +1,14 @@
+# Exercício 03 — Condições com if e else
+nome = input("Digite seu nome: ").strip()
+idade = int(input("Digite sua idade: "))
+idade_minima = 14
+
+print("\n--- VERIFICAÇÃO DE ACESSO ---")
+
+if idade >= idade_minima:
+    print(f"{nome}, seu acesso à oficina foi liberado.")
+    print("Você já possui a idade mínima exigida.")
+else:
+    anos_faltantes = idade_minima - idade
+    print(f"{nome}, seu acesso ainda não foi liberado.")
+    print(f"Faltam {anos_faltantes} ano(s) para atingir a idade mínima.")
